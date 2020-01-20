3c748f53da1f74fbe506e2b87bba8b74d495cac5
Heidi <hspradlin8@gmail.com>
2020-01-20 16:12:33 -0600

    employee, department, computer controller

---
+++ EFWorkForce/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFWorkForce.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set;  }
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+        [Display(Name = "Is Supervisor")]
+        public bool IsSupervisor { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        public string Email { get; set;  }
+        public int ComputerId { get; set; }
+        //navigation property 
+        public Computer Computer { get; set;  }
+    }
+}
